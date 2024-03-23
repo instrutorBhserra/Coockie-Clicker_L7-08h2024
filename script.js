856e95874906edbf387d22030989652dbcf8229b
Guilherme Hastenreiter Ferreira <instrutores.campinas@ctrlplay.online>
2024-03-23 09:18:49 -0300

    Feat: Add sound

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Constants
-const p = document.getElementById("target");
+const a = document.querySelector("audio");
+const p = document.querySelector("#target");
 
 // Variables
 let clicks = 0;
 
 // Functions
 function score() {
+    a.currentTime = 0;
+    a.play();
+
     clicks = clicks +1;
     p.innerHTML = clicks;
 }
